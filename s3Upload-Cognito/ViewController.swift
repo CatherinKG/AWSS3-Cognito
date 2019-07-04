119cc9dff3f7b17a044c4679ac011c075f9ca381
CATHERINE <catherine.kg@cabotsolutions.com>
2019-07-04 17:27:23 +0530

    first commit

---
+++ s3Upload-Cognito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  s3Upload-Cognito
+//
+//  Created by EVENTORG CATHERINE on 04/07/19.
+//  Copyright © 2019 Catherine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
